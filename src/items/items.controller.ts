5fd7f7c9a0f3022ff22dfd818c45ca2402f9bba5
irtizabatool <irtizabatool@gmail.com>
2021-03-16 20:21:10 +0530

    Using Request and Response of Express

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
-
+import { Request, Response } from 'express';
 @Controller('items')
 export class ItemsController {
   @Get()
-  findAll(): string {
-    return 'Get All items';
+  findAll(@Req() req: Request, @Res() res: Response): Response {
+    console.log(req.url);
+    return res.send('Hey There');
   }
 
   @Post()
