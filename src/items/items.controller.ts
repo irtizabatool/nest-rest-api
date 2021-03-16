1a95fbe046c50c1f56c07078fd7ad3d1d1de7d9a
irtizabatool <irtizabatool@gmail.com>
2021-03-16 20:33:47 +0530

    Added DELETE and PUT in the controller

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,16 +1,38 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
-import { Request, Response } from 'express';
+
 @Controller('items')
 export class ItemsController {
   @Get()
-  findAll(@Req() req: Request, @Res() res: Response): Response {
-    console.log(req.url);
-    return res.send('Hey There');
+  findAll(): string {
+    return 'Get All items';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Item ${id}`;
   }
 
   @Post()
   create(@Body() createItemDto: CreateItemDto): string {
     return `Name: ${createItemDto.name}, Description: ${createItemDto.description}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete ${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+    return `Update ${id} -- Name: ${updateItemDto.name}`;
   }
 }
