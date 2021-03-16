a75f8a031f4e886f3ba969facfb2ef5080cb1507
irtizabatool <irtizabatool@gmail.com>
2021-03-16 20:11:28 +0530

    Added Controller with GET and POST

---
+++ src/items/items.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+
+@Controller('items')
+export class ItemsController {
+  @Get()
+  findAll(): string {
+    return 'Get All items';
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name}, Description: ${createItemDto.description}`;
+  }
+}
